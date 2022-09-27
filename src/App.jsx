6753af9a5fe817a5574a52b3c21a0bc4f1514e15
Takatoshi0207 <t.kotani@m-okuchu.com>
2022-09-27 12:43:03 +0900

    Googleログインの設定

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import './App.css'
 import React from 'react'
 import SignIn from './components/SignIn'
+import { auth } from './firebase'
+import { useAuthState } from "react-firebase-hooks/auth"
 
 
 function App() {
-
+  const [user] = useAuthState(auth);
   return (
-    <div className="App">
+    <div>
+      {user ? <Line /> : <SingnOut />}
       <SignIn />
     </div>
   )
--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,8 +1,14 @@
 import { Button } from '@mui/material'
 import React from 'react'
+import firebase from 'firebase/compat/app'
+import { auth } from '../firebase';
 
 function SignIn() {
-  function signInWithGoogle() { }
+  function signInWithGoogle() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider);
+
+  }
 
   return (
     <div>
